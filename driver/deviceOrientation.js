cac64152e0529dd3aa58aa287db90729126c5f0c
Simon Bates <simon@bitstructures.com>
2016-09-23 16:02:17 -0400

    Add unsubscribe() to deviceOrientation.js

--- driver/deviceOrientation.js
+++ driver/deviceOrientation.js
@@ -1,20 +1,24 @@
 (function () {
-    var onDeviceAdded = function (deviceAddedCallback, options) {
-        var device = {
-            ID: "browserDeviceOrientation",
-            sensors: {
-                orientation: {
-                    subscribe: function (handler) {
-                        window.addEventListener("deviceorientation", handler, false);
-                    }
+    var device = {
+        ID: "BrowserDeviceOrientation",
+        sensors: {
+            orientation: {
+                subscribe: function (handler) {
+                    window.addEventListener("deviceorientation", handler, false);
+                },
+                unsubscribe: function (handler) {
+                    window.removeEventListener("deviceorientation", handler, false);
                 }
             }
-        };
+        }
+    };
+
+    var onDeviceAdded = function (deviceAddedCallback) {
         deviceAddedCallback(device);
     };
 
-    var onDeviceRemoved = function () {
-        // TODO
+    var onDeviceRemoved = function (deviceRemovedCallback) {
+        deviceRemovedCallback(device);
     };
 
     return {
